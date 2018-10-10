10aeec916cb1be567fa2ef5d3902efc8e38af9e0
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-10 20:10:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class ThreadTest
+    {
+        static void Main()
+        {
+            Thread t = new Thread(WriteY);
+            t.Start();
+            for (int i = 0; i < 20;i++)
+                Console.Write("X");
+        }
+        static void WriteY()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.Write("y");
+            Console.ReadLine();
+        }
+    }
+}
